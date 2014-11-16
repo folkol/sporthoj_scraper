8c808ca4f34e90a39263281547f469a490212375
Folkol <mattias4@kth.se>
2014-11-16 16:50:22 +0100

    Added catch for single paragraph post...

--- scraper.py
+++ scraper.py
@@ -7,7 +7,8 @@ from datetime import datetime
 
 
 def download_img(id):
-  img=urlopen('http://www.sporthoj.com/galleri/bild?id={0}&header=1'.format(id))
+  base_url = 'http://www.sporthoj.com/galleri/bild?id={0}&header=1'
+  img = urlopen(base_url.format(id))
   with open(id + '.jpg', "wb") as file:
     file.write(img.read())
 
@@ -21,7 +22,8 @@ def main(blog_name):
 
   for counter, post in enumerate(soup.find(id='inlagg')):
     title = post.div.text.strip()
-    byline = post.select('div')[3].text
+    byline = post.select('div[id=publicerad]')[0].text
+    print byline
     date = search(r'Tidpunkt:(.+)[|]', byline).group(1).strip()
     date = datetime.strptime(date, '%d / %m - %Y %H:%M').strftime('%y-%m-%d')
     filename_cleaned = sub('\W', '-', title)
@@ -30,8 +32,13 @@ def main(blog_name):
     makedirs(filename), chdir(filename)
     with open(filename_cleaned.encode('utf-8') + '.txt', 'w+') as file:
       print >> file, title.encode('utf-8'), '\n'
-      for paragraph in post.select('div font p'):
-        print >> file, paragraph.text.encode('utf-8').strip(), '\n'
+      paragraphs = post.select('div:nth-of-type(3) p')
+      if not paragraphs:
+          # Ever heard of semantic web?
+          p=post.select('div:nth-of-type(3)')
+          print >> file, p[0].text.encode('utf-8').strip(), '\n'
+      for p in paragraphs:
+        print >> file, p.text.encode('utf-8').strip(), '\n'
       for img in post.select('div:nth-of-type(2) a img'):
         print img['src']
         download_img(search(r'id=([0-9]+)[&]', img['src']).group(1))
